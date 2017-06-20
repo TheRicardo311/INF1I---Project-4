3c6e6dd5e43779c48270022985c180bb464f3851
Onno Hoeksema <onnohoeksema@gmail.com>
2017-06-20 12:23:59 +0200

    Xamarin megaproject added

---
+++ Cross platform app project 4/Cross platform app project 4/Cross_platform_app_project_4/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cross_platform_app_project_4
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
