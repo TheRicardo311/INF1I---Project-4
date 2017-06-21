20541401421c831477b95121b810b2f36b1fffec
ShaneVersluis <shaneversluis2791@gmail.com>
2017-06-21 21:58:11 +0200

    Added the Decorater class :)

---
+++ Projectapp/App1/App1.Android/Topping.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1.Droid
+{
+    abstract class Topping : IPizza
+    {
+        public IPizza tempPizza;
+        public Topping (IPizza newPizza)
+        {
+            tempPizza = newPizza;
+        }
+
+
+        public string getDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
