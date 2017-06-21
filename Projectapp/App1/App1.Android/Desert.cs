d65c677db16d07940667a85958b5411416195387
Perryvdberg <Perryvdberg@hotmail.com>
2017-06-21 15:27:41 +0200

    set/get names

---
+++ Projectapp/App1/App1.Android/Desert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1.Droid
+{
+    public class desert : IRecipeCategories
+    {
+        public void setName()
+        {
+            Console.WriteLine("Please enter the name of the recipe: ");
+            Console.ReadLine();
+            return desertName;
+        }
+        public string getName()
+        {
+            desertName = value;
+        }
+    }
+}
