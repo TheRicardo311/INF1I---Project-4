446bd75b093c05c0af51e87851d66e40c5cb1152
Onno Hoeksema <onnohoeksema@gmail.com>
2017-06-26 11:37:02 +0200

    nogmaals

--- Projectapp/App1/App1.Android/RecipeInfo.cs
+++ Projectapp/App1/App1.Android/RecipeInfo.cs
@@ -12,7 +12,68 @@ using Android.Widget;
 
 namespace App1.Droid
 {
-    class Class1
+    class RecipeInfo
     {
+    }
+
+    class Banana_split
+    {
+        //Will return a query for the database
+        NullReferenceException
+    }
+    class Milkshake
+    {
+        //Will return a query for the database
+        NullReferenceException
+    }
+    class Irish_coffee
+    {
+        //Will return a query for the database
+        NullReferenceException
+    }
+    class Biefstuk
+    {
+        //Will return a query for the database
+        NullReferenceException
+    }
+    class Kip_madras
+    {
+        //Will return a query for the database
+        NullReferenceException
+    }
+    class Chili_con_carne
+    {
+        //Will return a query for the database
+        NullReferenceException
+    }
+    class Zandkoekjes
+    {
+        //Will return a query for the database
+        NullReferenceException
+    }
+    class Cake
+    {
+        //Will return a query for the database
+        NullReferenceException
+    }
+    class Worstenbroodjes
+    {
+        //Will return a query for the database
+        NullReferenceException
+    }
+    class Omelet
+    {
+        //Will return a query for the database
+         NullReferenceException
+    }
+    class Brood
+    {
+        //Will return a query for the database
+         NullReferenceException
+    }
+    class Aardbeiensmoothie
+    {
+        //Will return a query for the database
+         NullReferenceException
     }
 }
