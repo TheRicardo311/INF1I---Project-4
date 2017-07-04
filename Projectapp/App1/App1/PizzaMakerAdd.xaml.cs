4fb64ee4c88a1fa15930a7ff07ccb4523b2a191d
Onno Hoeksema <onnohoeksema@gmail.com>
2017-07-04 15:21:48 +0200

    added menu for picking toppings

--- Projectapp/App1/App1/PizzaMakerAdd.xaml.cs
+++ Projectapp/App1/App1/PizzaMakerAdd.xaml.cs
@@ -12,9 +12,54 @@ namespace App1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PizzaMakerAdd : ContentPage
     {
+
+        //Creating a list of toppings, the dirty way
+        private readonly List<string> _toppings = new List<string>
+        {
+            "Mozzarella", "Chicken", "Shoarma", "Pepperoni"
+        };
+
         public PizzaMakerAdd()
         {
             InitializeComponent();
+
+            //Display the toppings
+
+            ListOfToppings.ItemsSource = _toppings;
+        }
+
+        // Hieronder staat alle meuk van de mainpage
+
+        private void Handle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Sets the search keywords to string
+            string keyword = MainSearchBar.Text;
+
+            // Sets the search results in a list of string
+            IEnumerable<string> searchResults = _toppings.Where(name => name.ToLower().Contains(keyword.ToLower()));
+
+            // Update list with the search results
+            ListOfToppings.ItemsSource = searchResults;
+        }
+        /// <summary>
+        /// Just a search button function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainSearchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// When you tap something from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TapDatTopping(object sender, ItemTappedEventArgs e)
+        {
+            Navigation.PushAsync(new PizzaPage());
         }
     }
 }
