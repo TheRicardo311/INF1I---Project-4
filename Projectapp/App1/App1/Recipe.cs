0e6ea114fe1462c8a1cd074a8cba5fa05f2e644f
Ricardo van Oldenbarneveld genaamd Witte Tullingh <r.vanoldenbarneveld@live.nl>
2017-07-05 11:35:59 +0100

    RecipeClasses

--- Projectapp/App1/App1/Recipe.cs
+++ Projectapp/App1/App1/Recipe.cs
@@ -7,68 +7,506 @@ namespace App1
 {
     abstract class Recipe
     {
-        
+        //Type declaration
+        string recipe_name;
+        string recipe_category;
+        string recipe_ingredients;
+        string recipe_description;
     }
-    class Banana_split : Recipe
+    class French_toast : Recipe
     {
-        private string recipe_id;
-        //Will return a query for the database
-        string Query = @"SELECT * FROM recipe WHERE recept_name = 'banana split')";
+        //Create and return recipe_name
+        public string ReturnNameFrench_toast()
+        {
+            string recipe_name = "French toast";
 
-            }
-    class Milkshake : Recipe
-    {
-        //Will return a query for the database
+            return recipe_name;
+        }
+        
+        public string ReturnCategoryFrench_toast()
+        {
+            string recipe_category = "Breakfast";
+
+            return recipe_category;
+        }
+        
+        public string ReturnIngredientsFrench_toast()
+        {
+            string recipe_ingredients = "Bob1";
+
+            return recipe_ingredients;
+        }
+        
+        public string ReturnDescriptionFrench_toast()
+        {
+            string recipe_description = "Henk1";
+
+            return recipe_description;
+        }
         
     }
-    class Irish_coffee : Recipe
+    class Pancakes : Recipe
     {
-        //Will return a query for the database
-        
+        public string ReturnNamePancakes()
+        {
+            string recipe_name = "Pancakes";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryPancakes()
+        {
+            string recipe_category = "Breakfast";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsPancakes()
+        {
+            string recipe_ingredients = "Bob2";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionPancakes()
+        {
+            string recipe_description = "Henk2";
+
+            return recipe_description;
+        }
     }
-    class Biefstuk : Recipe
+    class Scrambled_eggs : Recipe
     {
-        //Will return a query for the database
-        string Query = @"SELECT * FROM recipe WHERE recept_name = 'biefstuk')";
+        public string ReturnNameScrambled_eggs()
+        {
+            string recipe_name = "Scrambled eggs";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryScrambled_eggs()
+        {
+            string recipe_category = "Breakfast";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsScrambled_eggs()
+        {
+            string recipe_ingredients = "Bob3";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionScrambled_eggs()
+        {
+            string recipe_description = "Henk3";
+
+            return recipe_description;
+        }
     }
-    class Kip_madras : Recipe
+    class Croissants : Recipe
     {
-        //Will return a query for the database
-        
+        public string ReturnNameCroissants()
+        {
+            string recipe_name = "Croissants";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryCroissants()
+        {
+            string recipe_category = "Breakfast";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsCroissants()
+        {
+            string recipe_ingredients = "Bob4";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionCroissants()
+        {
+            string recipe_description = "Henk4";
+
+            return recipe_description;
+        }
     }
-    class Chili_con_carne : Recipe
+    class Chocolate_pudding : Recipe
     {
-        //Will return a query for the database
-        
+        public string ReturnNameChocolate_pudding()
+        {
+            string recipe_name = "Chocolate pudding";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryChocolate_pudding()
+        {
+            string recipe_category = "Desert";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsChocolate_pudding()
+        {
+            string recipe_ingredients = "Bob5";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionChocolate_pudding()
+        {
+            string recipe_description = "Henk5";
+
+            return recipe_description;
+        }
+        string recipe_name = "Chocolate pudding";
+        string recipe_category = "Desert";
+        string recipe_ingredients = "Bob5";
+        string recipe_description = "Henk5";
     }
-    class Zandkoekjes : Recipe
+    class Strawberry_pudding : Recipe
     {
-        //Will return a query for the database
-        string Query = @"SELECT * FROM recipe WHERE recept_name = 'zandkoekjes')";
+        public string ReturnNameStrawberry_pudding()
+        {
+            string recipe_name = "Strawberry pudding";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryStrawberry_pudding()
+        {
+            string recipe_category = "Desert";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsStrawberry_pudding()
+        {
+            string recipe_ingredients = "Bob6";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionStrawberry_pudding()
+        {
+            string recipe_description = "Henk6";
+
+            return recipe_description;
+        }
     }
-    class Cake : Recipe
+    class Vanilla_pudding : Recipe
     {
-        //Will return a query for the database
-        
+        public string ReturnNameVanilla_pudding()
+        {
+            string recipe_name = "Vanilla pudding";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryVanilla_pudding()
+        {
+            string recipe_category = "Desert";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsVanilla_pudding()
+        {
+            string recipe_ingredients = "Bob7";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionVanilla_pudding()
+        {
+            string recipe_description = "Henk7";
+
+            return recipe_description;
+        }
+
+        string recipe_name = "Vanilla pudding";
+        string recipe_category = "Desert";
+        string recipe_ingredients = "Bob7";
+        string recipe_description = "Henk7";
     }
-    class Worstenbroodjes : Recipe
+    class Tiramisu : Recipe
     {
-        //Will return a query for the database
-        
+        public string ReturnNameTiramisu()
+        {
+            string recipe_name = "Tiramisu";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryTiramisu()
+        {
+            string recipe_category = "Desert";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsTiramisu()
+        {
+            string recipe_ingredients = "Bob8";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionTiramisu()
+        {
+            string recipe_description = "Henk8";
+
+            return recipe_description;
+        }
+
+        string recipe_name = "Tiramisu";
+        string recipe_category = "Desert";
+        string recipe_ingredients = "Bob8";
+        string recipe_description = "Henk8";
     }
-    class Omelet : Recipe
+    class Brownies : Recipe
     {
-        //Will return a query for the database
-        string Query = @"SELECT * FROM recipe WHERE recept_name = 'omelet')";
+        public string ReturnNameBrownies()
+        {
+            string recipe_name = "Brownies";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryBrownies()
+        {
+            string recipe_category = "Snacks";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsBrownies()
+        {
+            string recipe_ingredients = "Bob9";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionBrownies()
+        {
+            string recipe_description = "Henk9";
+
+            return recipe_description;
+        }
     }
-    class Brood : Recipe
+    class Popcorn : Recipe
     {
-        //Will return a query for the database
-         
+        public string ReturnNamePopcorn()
+        {
+            string recipe_name = "Popcorn";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryPopcorn()
+        {
+            string recipe_category = "Snacks";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsPopcorn()
+        {
+            string recipe_ingredients = "Bob10";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionPopcorn()
+        {
+            string recipe_description = "Henk10";
+
+            return recipe_description;
+        }
     }
-    class Aardbeiensmoothie : Recipe
+    class Fudge : Recipe
     {
-        //Will return a query for the database
-         
+        public string ReturnNameFudge()
+        {
+            string recipe_name = "Fudge";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryFudge()
+        {
+            string recipe_category = "Snacks";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsFudge()
+        {
+            string recipe_ingredients = "Bob11";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionFudge()
+        {
+            string recipe_description = "Henk11";
+
+            return recipe_description;
+        }
+    }
+    class Nachos : Recipe
+    {
+        public string ReturnNameNachos()
+        {
+            string recipe_name = "Nachos";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryNachos()
+        {
+            string recipe_category = "Snacks";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsNachos()
+        {
+            string recipe_ingredients = "Bob12";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionNachos()
+        {
+            string recipe_description = "Henk12";
+
+            return recipe_description;
+        }
+    }
+    class Shephards_pie : Recipe
+    {
+        public string ReturnNameShephards_pie()
+        {
+            string recipe_name = "Shephards pie";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryShephards_pie()
+        {
+            string recipe_category = "Main dish";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsShephards_pie()
+        {
+            string recipe_ingredients = "Bob13";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionShephards_pie()
+        {
+            string recipe_description = "Henk13";
+
+            return recipe_description;
+        }
+    }
+    class Hamburger : Recipe
+    {
+        public string ReturnNameHamburger()
+        {
+            string recipe_name = "Hamburger";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryHamburger()
+        {
+            string recipe_category = "Main dish";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsHamburger()
+        {
+            string recipe_ingredients = "Bob14";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionHamburger()
+        {
+            string recipe_description = "Henk14";
+
+            return recipe_description;
+        }
+    }
+    class Chicken_soup : Recipe
+    {
+        public string ReturnNameChicken_soup()
+        {
+            string recipe_name = "Chicken soup";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryChicken_soup()
+        {
+            string recipe_category = "Main dish";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsChicken_soup()
+        {
+            string recipe_ingredients = "Bob15";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionChicken_soup()
+        {
+            string recipe_description = "Henk15";
+
+            return recipe_description;
+        }
+    }
+    class Pasta_bolognese : Recipe
+    {
+        public string ReturnNamePasta_bolognese()
+        {
+            string recipe_name = "Pasta bolognese";
+
+            return recipe_name;
+        }
+
+        public string ReturnCategoryPasta_bolognese()
+        {
+            string recipe_category = "Main dish";
+
+            return recipe_category;
+        }
+
+        public string ReturnIngredientsPasta_bolognese()
+        {
+            string recipe_ingredients = "Bob16";
+
+            return recipe_ingredients;
+        }
+
+        public string ReturnDescriptionPasta_bolognese()
+        {
+            string recipe_description = "Henk16";
+
+            return recipe_description;
+        }
     }
 }
