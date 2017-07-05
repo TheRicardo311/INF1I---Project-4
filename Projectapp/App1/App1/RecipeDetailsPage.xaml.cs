2758bb1e1cf385745f68bf14e3f1ae2088de7ce7
Khanh <0908532@users.noreply.github.com>
2017-07-05 14:27:00 +0200

    It's working yo

--- Projectapp/App1/App1/RecipeDetailsPage.xaml.cs
+++ Projectapp/App1/App1/RecipeDetailsPage.xaml.cs
@@ -12,31 +12,53 @@ namespace App1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RecipeDetailsPage : ContentPage
     {
-        public RecipeDetailsPage(string selectedRecipe)
+        public RecipeDetailsPage(string recipeName, string recipeCategory, string recipeIngredients, string recipeDescription)
         {
             InitializeComponent();
-            
-            // Set title of recipe
-            this.Title = selectedRecipe;
-            
 
-            // Set stacklayout
-            var layout = new StackLayout { Margin = 20 };
-            this.Content = layout;
+            // Set title of recipe
+            this.Title = recipeName;
+            this.BackgroundColor = Color.Black;
+
+
 
             // Set image
-            Image _image = new Image { Source = selectedRecipe + ".jpg" };
-
-            // Set description to label
-            Label _label = new Label
+            Image _image = new Image
             {
-                Text = "Add description here for " + selectedRecipe
+                Source = recipeName + ".jpg"
             };
 
+            // Set description as label
+            Label _label = new Label
+            {
+                Text = "Catergory" + "\n\n" + recipeCategory + "\n\n\n" + 
 
-            // Adding things to screen
-            layout.Children.Add(_image);
-            layout.Children.Add(_label);
+                    "Ingredients" + "\n\n" + recipeIngredients + "\n\n\n" +
+
+                    "Steps" + "\n\n" + recipeDescription + "\n", 
+
+
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                TextColor = Color.White
+            };
+
+            // Scroll view
+            ScrollView scrollView = new ScrollView
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Content = _label,
+                Margin = new Thickness(20, 10, 20, 20)
+            };
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    _image,
+                    scrollView
+                }
+            };
         }
     }
 }
